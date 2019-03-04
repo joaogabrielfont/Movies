45dfea3f1897fc65aba700a6d0979c2e1e1c5650
João Gabriel <joao.conceicao@monitoratec.com.br>
2019-03-04 11:50:14 -0300

    Initial Commit

---
+++ MoviesSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesSwift
+//
+//  Created by João Gabriel  on 04/03/2019.
+//  Copyright © 2019 fontana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
